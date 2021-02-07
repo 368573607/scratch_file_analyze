c4ef126a7fa66a9917043d600ad46a2502da26f3
redsun <xuhuading@foxmail.com>
2021-02-07 19:30:23 +0800

    Delete chinese_english
    删除未使用的中英对照对象，
    该功能将在可视化版本中发布，
    不在纯分析器中添加冗余内容

--- src/add.js
+++ src/add.js
@@ -1,16 +1,5 @@
 const matchs = require("./match");
 
-const block_english_and_chinese = {
-    "event": "事件",
-    "motion": "运动",
-    "looks": "外观",
-    "sound": "声音",
-    "control": "控制",
-    "sensing": "侦测",
-    "operator": "运算",
-    "data": "变量和列表"
-};
-
 let add = function (bgtAdd) {
     let returnList = {
         "event": 0,
