cac4c1337050ed81e26b66f967539f5765be5215
redsun <xuhuading@foxmail.com>
2021-02-11 16:46:46 +0800

    添加新的Api
    get_analyze: 得到分析后的数据
    get_blocks_quantity: 得到积木总数
    get_block: 得到某一个类型积木的总数

--- src/index.js
+++ src/index.js
@@ -1,16 +1,42 @@
 const add = require("./add");
 const get_opcode = require("./get_opcode");
 
-let analyze = function (json) {
+let get_analyze = (json)=> {
     return new Promise((resolve, reject) => {
         try{
-        let opcodes = get_opcode(json);
-        resolve(add(opcodes));
+            let opcodes = get_opcode(json);
+            resolve(add(opcodes));
         }catch(e){
-            reject(e);
+            reject(new Error(e));
         }
     })
-
 }
 
-module.exports = analyze;
+let get_blocks_quantity = (json)=>{
+    return new Promise((resolve,reject)=>{
+        get_analyze(json).then((result)=>{
+            let blocks = result;
+            resolve(Object.keys(blocks).length);
+        }).catch((err)=>{
+            reject(new Error(err));
+        });
+    })
+}
+
+let get_block = (json,block)=>{
+    return new Promise((resolve,reject)=>{
+        let allquantity = get_blocks_quantity(json);
+
+        get_analyze(json).then((result)=>{
+            resolve(result[block]);
+        }).catch((err)=>{
+            reject(new Error(err));
+        });
+    });
+}
+
+module.exports = {
+    get_analyze:get_analyze,
+    get_blocks_quantity:get_blocks_quantity,
+    get_block:get_block
+};
--- test/index.test.js
+++ test/index.test.js
@@ -1,8 +1,20 @@
 const anlyze = require("../src/index");
 const testjson = require("./test.json");
 
-anlyze(testjson).then((result)=>{
+anlyze.get_analyze(testjson).then((result)=>{
     console.log(result);
 }).catch((err)=>{
-    console.error(new Error(err));
+    console.error(err);
+});
+
+anlyze.get_blocks_quantity(testjson).then((result)=>{
+    console.log(result);
+}).catch((err)=>{
+    console.error(err);
+});
+
+anlyze.get_block(testjson,"event").then((result)=>{
+    console.log(result);
+}).catch((err)=>{
+    console.log(err);
 });
