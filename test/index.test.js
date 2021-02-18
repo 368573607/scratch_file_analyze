aca95e59e744963d8cc46327a1dfb9e2323edcf7
redsun <xuhuading@foxmail.com>
2021-02-18 19:08:20 +0800

    格式化代码

--- test/index.test.js
+++ test/index.test.js
@@ -1,20 +1,20 @@
 const anlyze = require("../src/index");
 const testjson = require("./test.json");
 
-anlyze.get_analyze(testjson).then((result)=>{
+anlyze.get_analyze(testjson).then((result) => {
     console.log(result);
-}).catch((err)=>{
+}).catch((err) => {
     console.error(err);
 });
 
-anlyze.get_blocks_quantity(testjson).then((result)=>{
+anlyze.get_blocks_quantity(testjson).then((result) => {
     console.log(result);
-}).catch((err)=>{
+}).catch((err) => {
     console.error(err);
 });
 
-anlyze.get_block(testjson,"event").then((result)=>{
+anlyze.get_block(testjson, "event").then((result) => {
     console.log(result);
-}).catch((err)=>{
+}).catch((err) => {
     console.log(err);
 });
